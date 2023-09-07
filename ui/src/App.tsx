df0f38cd506aaf8aaf0d11570bf52b4ae0cd6f90
felibuscaglia <felipebbuscaglia@gmail.com>
2023-09-07 18:48:33 -0300

    🎨 Adds react-router-dom

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MapCreatorScreen from "screens/MapCreator";
+
 const App = () => {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MapCreatorScreen />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ ui/src/screens/MapCreator.tsx
@@ -0,0 +1,5 @@
+const MapCreatorScreen = () => {
+  return <div>Map creator screen</div>;
+};
+
+export default MapCreatorScreen;

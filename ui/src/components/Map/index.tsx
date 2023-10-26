4aa12caebf90f4a6eaf3a48f67c7cc4a3df4fa70
felibuscaglia <felipebbuscaglia@gmail.com>
2023-10-26 17:22:52 -0300

    Adds geocoder

--- ui/src/components/Map/index.tsx
+++ ui/src/components/Map/index.tsx
@@ -1,8 +1,13 @@
 import "mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl, { Map } from "mapbox-gl";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import FeatureList from "./FeatureList";
 import { IMap } from "lib/interfaces/entities";
+import ReactDOMServer from "react-dom/server";
+import { Search } from "react-feather";
+import { SECONDARY_BRAND_COLOR } from "lib/constants/styles";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN || "";
 
@@ -10,23 +15,58 @@ const DEFAULT_ZOOM_LEVEL = 11;
 const DEFAULT_LNG = -70.9;
 const DEFAULT_LAT = 42.35;
 
+const initializeMap = (mapContainerRef: React.RefObject<HTMLDivElement>) => {
+  const map = new mapboxgl.Map({
+    container: mapContainerRef.current!,
+    style: "mapbox://styles/mapbox/streets-v12",
+    center: [DEFAULT_LNG, DEFAULT_LAT],
+    zoom: DEFAULT_ZOOM_LEVEL,
+  });
+
+  const geocoderContainer = document.createElement("div");
+
+  geocoderContainer.className =
+    "flex items-center top-20 fixed right-4 w-3/12 rounded-sm";
+
+  const searchBtn = document.createElement("button");
+  searchBtn.className = "h-10 bg-primary-brand-color px-4 relative rounded-r-sm";
+  const searchIconHTML = ReactDOMServer.renderToString(
+    <Search size={20} color={SECONDARY_BRAND_COLOR} />,
+  );
+  searchBtn.innerHTML = searchIconHTML;
+
+  const geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl,
+    placeholder: "Search for places around the world",
+  });
+
+  geocoder.on("result", (event) => {
+    const { center, place_name } = event.result;
+    new mapboxgl.Popup({ closeOnClick: true })
+      .setLngLat(center)
+      .setHTML(`<h1 className='font-title'>${place_name}</h1>`)
+      .addTo(map);
+  });
+
+  map.on("load", () => {
+    geocoderContainer.appendChild(geocoder.onAdd(map));
+    map.getContainer().appendChild(geocoderContainer);
+    geocoderContainer.appendChild(searchBtn);
+  });
+};
+
 interface IMapComponentProps {
   map: IMap;
 }
 
 const MapComponent: React.FC<IMapComponentProps> = ({ map }) => {
   const mapContainer = useRef<HTMLDivElement | null>(null);
-  const mapRef = useRef<Map | null>(null);
 
   useEffect(() => {
-    if (mapRef.current || !mapContainer.current) return;
+    if (!mapContainer.current) return;
 
-    mapRef.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v12",
-      center: [DEFAULT_LNG, DEFAULT_LAT],
-      zoom: DEFAULT_ZOOM_LEVEL,
-    });
+    initializeMap(mapContainer);
   }, [map]);
 
   return (
